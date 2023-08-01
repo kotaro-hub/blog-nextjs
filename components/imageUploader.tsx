e55a071a801419ccec3140b857ad5129a478c8a8
kotarokomori <komopro.works@gmail.com>
2023-08-02 00:00:38 +0900

    画像追加機能と削除機能を作成

--- components/imageUploader.tsx
+++ components/imageUploader.tsx
@@ -1,21 +1,32 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react'
+import { Button, Image, FormControl, FormLabel } from "@chakra-ui/react"
 
 function ImageUploader() {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState<string>("")
+  const fileInputRef = useRef<HTMLInputElement>(null)
 
-  const onImageChange = (event: any) => {
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      let img = event.target.files[0];
-      setImage(URL.createObjectURL(img));
+      let img = event.target.files[0]
+      setImage(URL.createObjectURL(img))
+    }
+  }
+
+  const onImageRemove = () => {
+    setImage("")
+    if (fileInputRef.current) {
+      fileInputRef.current.value=""
     }
   }
 
   return (
-    <div>
-      <input type="file" name="myImage" onChange={onImageChange} />
-      <img src={image} />
-    </div>
+    <FormControl>
+      <FormLabel>画像</FormLabel>
+      <input type="file" onChange={onImageChange} ref={fileInputRef} />
+      {image && <Image src={image} />}
+      {image && <Button onClick={onImageRemove}>Remove image</Button>}
+    </FormControl>
   )
 }
 
-export default ImageUploader;
+export default ImageUploader
