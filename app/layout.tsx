0a62eefd81b6c38cbbb89bc3474d70cbe0b4e28a
kotarokomori <komopro.works@gmail.com>
2023-08-14 15:40:32 +0900

    getServerSession

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import "./globals.css"
+import { getServerSession } from "next-auth"
 import { Roboto } from "next/font/google"
 
 import Provider from "@/app/components/provider"
 import SessionProvider from "@/app/components/sessionProvider"
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -17,6 +19,7 @@ export const metadata = {
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const session = getServerSession(authOptions)
   
   return (
     <html lang="ja">
